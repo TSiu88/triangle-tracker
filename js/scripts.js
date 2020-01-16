84f3b9bd7ca998685188901afba65fbe15d49cf8
Erik Grove and Tiffany Siu <tiffany@>
2020-01-16 15:08:50 -0800

    add js logic with alerts for type of triangle

---
+++ js/scripts.js
@@ -0,0 +1,41 @@
+function sideCompare(side1, side2, side3) {
+  if(Number.isNaN(side1) || Number.isNaN(side2) || Number.isNaN(side3)){
+    alert("This is not a triangle. Triangles are defined by numbers");
+  }
+  else if((side1+side2) <= side3){
+    alert("This does not make a triangle")
+  }
+  else{
+    if(side1 === side2) {
+      if(side1 === side3){
+        alert("This triangle is Equilateral!");
+      }
+      else{
+        alert("This triangle is Isosceles!");
+      }
+    }
+    else if(side1 === side3 || side2 === side3){
+      alert("This triangle is Isosceles!");
+    }
+    else{
+      alert("This triangle is Scalene!")
+    }
+  }
+}
+
+
+$(document).ready(function(){
+  $("form").submit(function(event){
+
+  var side1 = parseInt($("#side1").val());
+  var side2 = parseInt($("#side2").val());
+  var side3 = parseInt($("#side3").val());
+  console.log(side2);
+
+
+  sideCompare(side1, side2, side3);
+  
+  event.preventDefault();
+
+  });
+});
